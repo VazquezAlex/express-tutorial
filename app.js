1600a00ee16376a4f8b7bc6c5154f8bbd5b53481
VazquezAlex <alejandrovazquez.dev@gmail.com>
2023-09-19 13:16:22 -0600

    feature: created express app

---
+++ app.js
@@ -0,0 +1,7 @@
+// Third party imports.
+const express = require('express');
+
+// Create express app.
+const app = express();
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,6 @@
+// Local imports.
+const app = require('./app');
+
+app.listen(3500, () => {
+    console.log('The server is live');
+});
